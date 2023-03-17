54679ae5d55fcb127310b7c217c52120f4494e3d
Liisa <liisa.pent@tktk.ee>
2023-03-17 19:27:34 +0200

    valmis

---
+++ Week8EpicGame/Program.cs
@@ -0,0 +1,32 @@
+﻿string folderPath = @"C:\Users\pentl\Desktop\TKTK\IVsemester\ProgeAlused\data\";
+string heroFile = "heroes.txt";
+string villainFile = "villains.txt";
+
+string[] heroes = File.ReadAllLines(Path.Combine(folderPath, heroFile));
+string[] villains = File.ReadAllLines(Path.Combine(folderPath, villainFile));
+
+//string[] heroes = { "Superman", "Batman", "Catwoman", "Spiderman" };
+//string[] villains = { "Joker", "Megamind", "Thanos", "Candyman", "Dracula" };
+
+
+string[] weapon = { "plastic fork", "banana", "lego brick", "sword", "gun" };
+
+string hero = GetRandomValueFromArray(heroes);
+string heroWeapon = GetRandomValueFromArray(weapon);
+Console.WriteLine($"Today {hero} with {heroWeapon} saves the day!");
+
+string villain = GetRandomValueFromArray(villains);
+string villainWeapon = GetRandomValueFromArray(weapon);
+Console.WriteLine($"Today {villain} with {villainWeapon} tries to take over the world!");
+
+
+static string GetRandomValueFromArray(string[] someArray)
+{
+    Random rnd = new Random();
+    int randomIndex = rnd.Next(0, someArray.Length);
+    string randomStringFromArray = someArray[randomIndex];
+    return randomStringFromArray;
+}
+
+
+
